0d39b5d7ea59b3be00cd1677db696b4090baee19
Jephunneh <52535123+mcjeph@users.noreply.github.com>
2020-01-12 09:47:26 +0800

    update 01/12/19

--- iLearnCentral/app/src/main/java/com/project/ilearncentral/ActivityPages.java
+++ iLearnCentral/app/src/main/java/com/project/ilearncentral/ActivityPages.java
@@ -3,27 +3,38 @@ package com.project.ilearncentral;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.viewpager.widget.ViewPager;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ActivityPages extends AppCompatActivity implements View.OnClickListener {
 
     ViewPager viewPager;
     TabLayout tabLayout;
+    Button featuresButton, searchButton;
+    TextView searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pages);
 
-        Button backButton = (Button)findViewById(R.id.back_button);
-        backButton.setOnClickListener(this);
+        featuresButton = (Button)findViewById(R.id.features_button);
+        searchButton = (Button)findViewById(R.id.search_button);
+        searchBar = (TextView) findViewById(R.id.search_bar);
+        featuresButton.setOnClickListener(this);
+        searchButton.setOnClickListener(this);
 
         viewPager = (ViewPager)findViewById(R.id.htab_viewpager);
         tabLayout = (TabLayout)findViewById(R.id.htab_tabs);
@@ -39,8 +50,11 @@ public class ActivityPages extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.back_button:
+            case R.id.features_button:
                 startActivity(new Intent(getApplicationContext(), Home.class));
+                break;
+            case R.id.search_button:
+                searchBar.setVisibility(View.VISIBLE);
                 break;
             default:
                 return;
--- iLearnCentral/app/src/main/java/com/project/ilearncentral/Home.java
+++ iLearnCentral/app/src/main/java/com/project/ilearncentral/Home.java
@@ -1,5 +1,6 @@
 package com.project.ilearncentral;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,21 +11,22 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
+
+    Button backButton;
+    LinearLayout userPageLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button backButton = (Button)findViewById(R.id.back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        backButton = (Button)findViewById(R.id.back_button);
+        userPageLink = (LinearLayout)findViewById(R.id.user_page_link);
+        backButton.setOnClickListener(this);
+        userPageLink.setOnClickListener(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,4 +38,16 @@ public class Home extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back_button:
+                onBackPressed();
+                break;
+            case R.id.user_page_link:
+                startActivity(new Intent(getApplicationContext(), ActivityPages.class));
+            default:
+                return;
+        }
+    }
 }
